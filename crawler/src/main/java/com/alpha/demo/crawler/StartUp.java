54f165ee823ceeb2cdd809b3030263303d7b96b6
sliu <sliu@coupang.com>
2014-07-09 08:44:44 +0800

    merge 5

--- crawler/src/main/java/com/alpha/demo/crawler/StartUp.java
+++ crawler/src/main/java/com/alpha/demo/crawler/StartUp.java
@@ -8,6 +8,6 @@ public class StartUp {
     public static void main( String[] args ) {
     	LogUtil.initLog4j();
     	new Worker(Consts.CRAWL_URL).start();
-        // change testing 3
+        // change testing 5
     }
 }
